31ce4b0c393b6477cae648ef03bced7601031d60
Dario Pettinella <dario.pettinella@gmail.com>
2024-02-16 12:26:26 +0000

    Remove settings from gitignore. Added UART Test.

--- DST5-Source/Core/Src/main.c
+++ DST5-Source/Core/Src/main.c
@@ -105,11 +105,12 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET); // RED LED ON
+	  HAL_Delay(1000); // Delay in milliseconds
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET); //  RED LED OFF
+	  HAL_Delay(1000); // Delay in milliseconds
+	  HAL_UART_Transmit(&huart1, "Hello World!\n", 14, 100);
     /* USER CODE END WHILE */
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET); // RED LED ON
-	HAL_Delay(2000); // Delay in milliseconds
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET); //  RED LED OFF
-	HAL_Delay(2000); // Delay in milliseconds
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -221,7 +222,7 @@ static void MX_USART1_UART_Init(void)
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_7B;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
   huart1.Init.Mode = UART_MODE_TX_RX;
